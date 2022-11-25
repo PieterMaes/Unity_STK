11527066805f64bb04e84456732afd0004ee0176
Jeanke2001 <jan.joostens@student.kuleuven.be>
2022-11-25 14:17:39 +0100

    testing Tiles

--- Assets/Project/Scripts/Game/CollisionKiller.cs
+++ Assets/Project/Scripts/Game/CollisionKiller.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionKiller : MonoBehaviour
 {
+    [Header("AddPoints")]
+    public int value_cats; 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "LavaOrb")
         {
-
+            SceneManager.LoadScene("Death");
         }
 
         if (collision.gameObject.tag == "LavaWave")
         {
-
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(
+            0, 0
+            );
         }
 
         if (collision.gameObject.tag == "Cat")
         {
-
+            //add the wanted points of this prefab tp score, located in scoretext script
+            GameObject.Find("ScoreText").transform.GetComponent<ScoreText>().Score += value_cats;
+            
+            //kill object
+            Destroy(collision.gameObject);
         }
         //Tagging objects
     }
