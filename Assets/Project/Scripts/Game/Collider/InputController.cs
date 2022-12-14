501430d2a6c2ea751111f60a65d4f4eea8fc2018
Kato Wildiers <kato.wildiers@student.kuleuven.be>
2022-12-14 11:28:09 +0100

    Revert "Merge branch 'main' of https://github.com/PieterMaes/Unity_STK"
    This reverts commit f314082a46682c5f908fbb03195ed0117fc5a01f, reversing
    changes made to 3da776defe321e66d89998f54efdc6cf90abc595.

--- Assets/Project/Scripts/Game/Collider/InputController.cs
+++ Assets/Project/Scripts/Game/Collider/InputController.cs
@@ -17,29 +17,10 @@ public class InputController : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
-        string the_com1 = "COM3";
-        //string the_com2 = "COM4";
-        sp2 = new SerialPort(the_com1, 9600);
-        //sp1 = new SerialPort(the_com2, 9600);
-        /*
-        if (!sp1.IsOpen)
-        {
-            sp1.Open();
-            print("opened sp1");
-        }
-        */
-        if (!sp2.IsOpen)
-        {
-            sp2.Open();
-            print("opened sp2");
-        }
-=======
         string the_com1 = "COM5";
         string the_com2 = "COM6";
         sp1 = new SerialPort(the_com1, 9600);
         sp2 = new SerialPort(the_com2, 9600);
->>>>>>> 3463da47e87f34191710db6d206890290ab12d40
     }
 
     void CheckTiles() {
@@ -57,23 +38,11 @@ public class InputController : MonoBehaviour
 
         tile = GameObject.Find(substr);
         tile.GetComponent<HitScript>().hitTile(hardTrue);
-<<<<<<< HEAD
-    }   
-
-   
-=======
     }
->>>>>>> 3463da47e87f34191710db6d206890290ab12d40
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        if (!sp2.IsOpen)
-        {
-            sp2.Open();
-            print("opened sp2");
-=======
         if (!sp1.IsOpen){
             sp1.Open();
             print("opened sp");
@@ -105,37 +74,6 @@ public class InputController : MonoBehaviour
                 CheckTiles();
                 Debug.Log(message);
             }
->>>>>>> 3463da47e87f34191710db6d206890290ab12d40
         }
-        string message = "";
-        /*
-        if (sp1.BytesToRead > 0)
-        {
-            message = sp1.ReadLine();
-            inputString = message;
-            Debug.Log(message);
-        }*/
-        if (sp2.BytesToRead > 0)
-            {
-                message = sp2.ReadLine();
-                inputString = message;
-                Debug.Log(message);
-            }
-            /*
-            if (!sp1.IsOpen)
-            {
-                sp1.Open();
-                print("opened sp1");
-            }*/
-            
-
-            if (sp2.IsOpen)
-            {
-               CheckTiles();
-            }
-
-            }
-
-        
-    
+    }
 }
